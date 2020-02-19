29c614c8a14eb290555bd1708fafea6542365e60
Cooper Lees <cooper@fb.com>
2020-02-18 17:33:45 -0800

    Bump public version for PyPI release
    Summary: - Lets release this to PyPI with an accurate version
    
    Reviewed By: fried
    
    Differential Revision: D19959187
    
    fbshipit-source-id: a154a10582ddcd6e3adccd6a868e10bdfca37946

--- later/__init__.py
+++ later/__init__.py
@@ -18,7 +18,7 @@ from .event import BiDirectionalEvent
 from .task import START_TASK, Watcher, WatcherError, as_task, cancel
 
 
-__version__ = "20.01.0"
+__version__ = "20.2.0"
 __all__ = [
     "BiDirectionalEvent",
     "START_TASK",
