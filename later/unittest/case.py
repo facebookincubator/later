48616c510862571c7dc1125e311294f43e4936f0
generatedunixname89002005307016 <generatedunixname89002005307016@fb.com>
2020-07-21 08:36:06 -0700

    suppress errors in `later` - batch 1
    Reviewed By: pradeep90
    
    Differential Revision: D22628829
    
    fbshipit-source-id: ce4a9a62b21973274c710a44e5533b33012fe5c9

--- later/unittest/case.py
+++ later/unittest/case.py
@@ -21,8 +21,6 @@ from __future__ import annotations
 import asyncio
 import asyncio.coroutines
 import asyncio.futures
-
-# pyre-fixme[21]: Could not find `log`.
 import asyncio.log
 import asyncio.tasks
 import unittest.mock as mock
