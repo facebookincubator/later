fd650262d7492518488936dddadece9068a4ac07
Amethyst Reese <amethyst@n7.gg>
2023-11-29 16:20:26 -0800

    Replace setuptools/setup.py with PEP621 metadata and flit
    Fixes #13

--- later/__init__.py
+++ later/__init__.py
@@ -12,6 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+"""A toolbox for asyncio services"""
+
 from async_timeout import timeout
 
 from .event import BiDirectionalEvent
