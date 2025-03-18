7b61ba9bb6ce2a2e1e7133fc3f30e9c8ba370560
Paul O'Shannessy <poshannessy@meta.com>
2025-03-18 14:56:04 -0700

    Update copyright/license headers
    Summary:
    1. Update to Meta Platforms
    2. Consistently conform to header templates
    
    Reviewed By: fried, bigfootjon
    
    Differential Revision: D71414365
    
    fbshipit-source-id: 0a80c7268571d1a3b9f475066e72f40a7b411ae7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,16 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-# Configuration file for the Sphinx documentation builder.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # -- Path setup --------------------------------------------------------------
 
